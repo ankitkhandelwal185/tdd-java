b4cb8eacf4a685d8e801e8bdb419f54668577966
ankitk02 <ankit.khandelwal@kuliza.com>
2020-12-08 19:48:23 +0530

    updated regex

--- tdd-java/src/main/java/StringCalculator.java
+++ tdd-java/src/main/java/StringCalculator.java
@@ -1,12 +1,6 @@
 public class StringCalculator {
     public int add(String numbers) {
-        String nums = numbers
-                .replaceAll("###", "")
-                .replaceAll("\n", "")
-                .replaceAll("[A-za-z]", "")
-                .replaceAll("//", "")
-                .replaceAll(";", "")
-                .replaceAll(",", "");
+        String nums = numbers.replaceAll("[A-za-z###//;,\n]", "");
         Integer total = 0;
         for (int i = 0; i < nums.length(); i++) {
             total += Integer.parseInt(String.valueOf(nums.charAt(i)));
